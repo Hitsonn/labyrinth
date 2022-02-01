bd95148310af754bbe7525fa3dc7aa582ca302e1
HicenkoAI <hisenkoai@yandexlyceum.ru>
2022-02-01 10:06:00 +0300

    distribution of code

---
+++ constants.py
@@ -0,0 +1,17 @@
+WINDOWS_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 672, 648
+FPS = 10
+MAPS_DIR = "maps"
+TILE_SIZE = 32
+ENEMY_EVENT_TYPE = 30
+NUMBER_OF_LEVELS = 9
+INTRO_TEXT = ["PACMAN 2022", "", "", "", "",
+                  "", "", "", "", "",
+                  "для продолжения",
+                  "нажми любую клавишу...",
+                  ]
+FINAL_TEXT = ["PACMAN 2022", "", "", "", "",
+                  "", "", "", "",
+                  "спасибо за игру",
+                  "для выхода",
+                  "нажми любую клавишу...",
+                  ]
--- labyrinth.py
+++ labyrinth.py
@@ -1,198 +1,16 @@
 import sys
-import time
 
-import pytmx
 from audio import *
 
+from constants import *
+
+from personage import Hero, Enemy, Bonus, Labyrinth, Game
+
 pygame.init()
+
 pygame.mixer.init()
 
-WINDOWS_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 672, 648
-FPS = 10
-MAPS_DIR = "maps"
-TILE_SIZE = 32
-ENEMY_EVENT_TYPE = 30
-NUMBER_OF_LEVELS = 9
-INTRO_TEXT = ["PACMAN 2022", "", "", "", "",
-                  "", "", "", "", "",
-                  "для продолжения",
-                  "нажми любую клавишу...",
-                  ]
-FINAL_TEXT = ["PACMAN 2022", "", "", "", "",
-                  "", "", "", "",
-                  "спасибо за игру",
-                  "для выхода",
-                  "нажми любую клавишу...",
-                  ]
-IS_PAUSED = False
-
-
-class Labyrinth:
-
-    def __init__(self, filename, free_tiles, finish_tile):
-        self.map = pytmx.load_pygame(f"{MAPS_DIR}/{filename}")
-        self.height = self.map.height
-        self.width = self.map.width
-        self.tile_size = self.map.tilewidth
-        self.free_tiles = free_tiles
-        self.finish_tile = finish_tile
-
-    def render(self, screen):
-        for y in range(self.height):
-            for x in range(self.width):
-                image = self.map.get_tile_image(x, y, 0)
-                screen.blit(image, (x * self.tile_size, y * self.tile_size))
-
-    def get_tile_id(self, position):
-        return self.map.tiledgidmap[self.map.get_tile_gid(*position, 0)]
-
-    def is_free(self, position):
-        return self.get_tile_id(position) in self.free_tiles
-
-    def find_path_step(self, start, target):
-        INF = 1000
-        x, y = start
-        distance = [[INF] * self.width for _ in range(self.height)]
-        distance[y][x] = 0
-        prev = [[None] * self.width for _ in range(self.height)]
-        queue = [(x, y)]
-        while queue:
-            x, y = queue.pop(0)
-            for dx, dy in (1, 0), (0, 1), (-1, 0), (0, -1):
-                next_x, next_y = x + dx, y + dy
-                if 0 <= next_x < self.width and 0 < next_y < self.height and \
-                        self.is_free((next_x, next_y)) and distance[next_y][next_x] == INF:
-                    distance[next_y][next_x] = distance[y][x] + 1
-                    prev[next_y][next_x] = (x, y)
-                    queue.append((next_x, next_y))
-        x, y = target
-        if distance[y][x] == INF or start == target:
-            return start
-        while prev[y][x] != start:
-            x, y = prev[y][x]
-        return x, y
-
-
-class Hero:
-
-    def __init__(self, pic, position):
-        self.x, self.y = position
-        self.image = pygame.image.load(f"images/{pic}")
-
-    def get_position(self):
-        return self.x, self.y
-
-    def set_position(self, position):
-        self.x, self.y = position
-
-    def render(self, screen):
-        delta = (self.image.get_width() - TILE_SIZE) // 2
-        screen.blit(self.image, (self.x * TILE_SIZE - delta, self.y * TILE_SIZE - delta))
-
-
-class Enemy:
-
-    def __init__(self, pic, position, delay=300):
-        self.x, self.y = position
-        self.delay = delay
-        pygame.time.set_timer(ENEMY_EVENT_TYPE, self.delay)
-        self.image = pygame.image.load(f"images/{pic}")
-
-    def get_position(self):
-        return self.x, self.y
-
-    def set_position(self, position):
-        if not IS_PAUSED:
-            self.x, self.y = position
-
-    def render(self, screen):
-        delta = (self.image.get_width() - TILE_SIZE) // 2
-        screen.blit(self.image, (self.x * TILE_SIZE - delta, self.y * TILE_SIZE - delta))
-
-
-class Bonus:
-
-    def __init__(self, position):
-        self.x, self.y = position
-        self.image = pygame.image.load(f"images/close_bonus.png")
-
-    def get_position(self):
-        return self.x, self.y
-
-    def set_position(self, position):
-        if not IS_PAUSED:
-            self.x, self.y = position
-
-    def render(self, screen):
-        delta = (self.image.get_width() - TILE_SIZE) // 2
-        screen.blit(self.image, (self.x * TILE_SIZE - delta, self.y * TILE_SIZE - delta))
-
-    def activation(self):
-        self.image = pygame.image.load(f"images/open_bonus.png")
-
-
-class Game:
-    def __init__(self, labyrinth, hero, enemy, bonus):
-        self.labyrinth = labyrinth
-        self.hero = hero
-        self.enemy = enemy
-        self.bonus = bonus
-        self.score = 0
-        self.result = False
-        self.all_sprites = pygame.sprite.Group()
-
-    def render(self, screen):
-        self.labyrinth.render(screen)
-        self.hero.render(screen)
-        for bonus in self.bonus:
-            bonus.render(screen)
-        for enemy in self.enemy:
-            enemy.render(screen)
-
-    def update_hero(self):
-        next_x, next_y = self.hero.get_position()
-        if pygame.key.get_pressed()[pygame.K_LEFT]:
-            next_x -= 1
-        if pygame.key.get_pressed()[pygame.K_RIGHT]:
-            next_x += 1
-        if pygame.key.get_pressed()[pygame.K_UP]:
-            next_y -= 1
-        if pygame.key.get_pressed()[pygame.K_DOWN]:
-            next_y += 1
-        if self.labyrinth.is_free((next_x, next_y)):
-            self.hero.set_position((next_x, next_y))
-
-    def move_enemy(self):
-        for enemy in self.enemy:
-            next_position = self.labyrinth.find_path_step(enemy.get_position(),
-                                                          self.hero.get_position())
-            enemy.set_position(next_position)
-
-    def move_bonus(self):
-        for bonus in self.bonus:
-            next_position = self.labyrinth.find_path_step(bonus.get_position(),
-                                                          self.hero.get_position())
-            bonus.set_position(next_position)
-
-    def check_win(self):
-        pygame.mixer.music.stop()
-        return self.labyrinth.get_tile_id(self.hero.get_position()) == self.labyrinth.finish_tile
-
-    def check_lose(self):
-        for enemy in self.enemy:
-            if self.hero.get_position() == enemy.get_position():
-                self.result = True
-                pygame.mixer.music.stop()
-        return self.result
-
-    def check_bonus(self):
-        for bonus in self.bonus:
-            if self.hero.get_position() == bonus.get_position():
-                self.score = 100
-                bonus.activation()
-
-    def check_score(self):
-        return self.score
+all_bonus = []
 
 
 def play_audio(track):
@@ -255,6 +73,7 @@ def show_message(screen, message):
 
 
 def generate_level(level):
+    global all_bonus
     level = level
     speed_enemy = 300
     labirinth_1 = Labyrinth("map1.tmx", [10, 46], 46)
@@ -302,7 +121,6 @@ def terminate():
 
 
 def main():
-    global IS_PAUSED
     score = 0
     pygame.init()
     screen = pygame.display.set_mode(WINDOWS_SIZE)
@@ -310,6 +128,7 @@ def main():
     start_sound.play()
     start_screen(screen, INTRO_TEXT, '1')
     game_over = False
+    is_paused = False
     for i in range(1, NUMBER_OF_LEVELS + 1):
         show_score(screen, f" Bonus = {score}")
         pygame.display.flip()
@@ -323,12 +142,12 @@ def main():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     terminate()
-                if event.type == ENEMY_EVENT_TYPE and not game_over:
+                if event.type == ENEMY_EVENT_TYPE and not game_over and not is_paused:
                     game.move_enemy()
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_p:
-                        IS_PAUSED = not IS_PAUSED
-            if not IS_PAUSED:
+                        is_paused = not is_paused
+            if not is_paused:
                 if not game_over:
                     game.update_hero()
                 screen.fill((0, 0, 0))
@@ -358,7 +177,8 @@ def main():
                     show_message(screen, "You lose!")
                 pygame.display.flip()
                 clock.tick(FPS)
-        score += game.score
+        for bonus in all_bonus:
+            score += bonus.score
         level_sound.stop()
 
     pygame.quit()
